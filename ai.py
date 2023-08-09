386099f51cc58f8929184dc789e9878096df4d80
Dishant Sharma <105786803+sharmadishant04@users.noreply.github.com>
2023-08-09 22:56:08 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -12,7 +12,7 @@ import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
 
-# Creating the architecture of the Neural Network
+
 
 class Network(nn.Module):
     
